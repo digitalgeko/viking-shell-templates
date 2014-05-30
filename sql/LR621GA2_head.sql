142c11963d70d440e587b8b71411fffdf077ce50
Mardo Del Cid <mdelcid@digitalgeko.com>
2014-05-30 16:15:13 -0600

    fixing large sql file problem, using viking 0.3.14

--- sql/LR621GA2_head.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE  IF NOT EXISTS `${projectName}` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `${projectName}`;
--- MySQL dump 10.13  Distrib 5.5.16, for osx10.5 (i386)
---
--- Host: 127.0.0.1    Database: ${projectName}
--- ------------------------------------------------------
--- Server version	5.1.61
