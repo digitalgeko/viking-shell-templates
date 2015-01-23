b8ee4bac9ee8ba1502c205021f1a79e4861a6f22
Mardo Del Cid <mdelcid@digitalgeko.com>
2015-01-23 14:59:58 -0600

    Escaping dollar sign on angular init.js

--- baseConf/.templates/public/js/angular/init.js
+++ baseConf/.templates/public/js/angular/init.js
@@ -2,7 +2,7 @@ angular.module('${projectName}App', ["viking.angular"]);
 
 angular.element(document).ready(function() {
 	angular.forEach(angular.element(".portlet"), function (portletSection) {
-		if (portletSection.id.match("${projectName}$")) {
+		if (portletSection.id.match("${projectName}\$")) {
 			angular.bootstrap(portletSection, ['${projectName}App']);	
 		};
 	});	
