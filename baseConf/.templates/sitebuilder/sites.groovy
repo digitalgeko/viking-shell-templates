da1f8cf76c70ed97c1ddad20b6c2d69777fdd4c5
Mardo Del Cid <mardodcp@gmail.com>
2015-02-26 12:16:53 -0600

    Fixing liferay initialization and sites.groovy issues

--- baseConf/.templates/sitebuilder/sites.groovy
+++ baseConf/.templates/sitebuilder/sites.groovy
@@ -1,5 +1,13 @@
 def build(b) {
-	b.site("${projectName}") {
-		friendlyURL ""		
+	b.site("Guest") {
+		layout ("Home") {
+			friendlyURL "/home"
+			override true
+			isPrivateLayout false
+			layoutTemplateId "1_column"
+			column (1) {
+				portlet "${projectName.toLowerCase()}portlet_WAR_${projectName}"
+			}
+		}
 	}
 }
